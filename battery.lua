ed4078aad4b5056bffe6c134167eb22889b509c8
Eric <eric.nobody@gmail.com>
2016-01-16 00:08:30 -0500

    comments

--- battery.lua
+++ battery.lua
@@ -1,21 +1,26 @@
 local template = {}
 root = template
 
+local uis = {}
+template.uis = uis
+
+---------------------
+--Private variables--
+---------------------
 local title = 'BATTERY'
 local locked = false
 local signal = BUNDLE:new("back",colors.yellow,"battery_charged")
 local resetButton = BUNDLE:new("back",colors.white,"reset_button")
 
-local uis = {}
-template.uis = uis
-
+---------------------
+--Private functions--
+---------------------
 local function writeScreen(ui)
   ui.setBackgroundColor(colors.gray)
   ui.clear()
   ui.setBackgroundColor(colors.lightGray)
   ui:wipe(7,1,1)
   ui:wipe(7,1,5)
-  
   ui.setBackgroundColor(colors.lightGray)
   ui.setTextColor(colors.gray)
   ui:indentLeft(title,0,1)
@@ -43,7 +48,6 @@ local function chargeListener()
   end
 end
 
-
 local function reset()
   while true do
     local event = waitSignal("redstone")
@@ -59,12 +63,21 @@ local function reset()
   end
 end
 
+---------------------------
+--Initialization function--
+---------------------------
+--sets everything up 
 template.init = function()
   signal:disable()
   local ui = UI:aquireAnyMonitor()
     table.insert(uis,ui)
 end
 
+-----------------
+--Main function--
+-----------------
+--the function that starts execution
+--usual contains a loop that repeats a behaviour
 template.main = function()
   writeScreen(uis[1])
   runProcess(chargeListener,"charge_listener")

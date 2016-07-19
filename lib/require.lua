967f254a3a7b3117d2c9d77863e63b9327084b91
Eric Kemp <arqiteks@gmail.com>
2016-07-19 16:28:17 +0300

    Require loads modules into _ENV

--- lib/require.lua
+++ lib/require.lua
@@ -1,19 +1,52 @@
-local require = function(...)
-  for _,file in ipairs(arg) do
-    if fs.exists(file..".lua") then
-      return dofile(file..".lua")
-    elseif fs.exists("server/"..file..".lua") then
-      return dofile("server/"..file..".lua")
-    elseif fs.exists("arq/"..file..".lua") then
-      return dofile("arq/"..file..".lua")
-    elseif fs.exists("arq/src/"..file..".lua") then
-      return dofile("arq/src/"..file..".lua")
-    elseif fs.exists("arq/lib/"..file..".lua") then
-      return dofile("arq/lib/"..file..".lua")
+local function checkFileExists(path)
+  return fs.exists(path)
+end
+
+local require = function(name)
+  local ENV = getfenv(2)
+  local loadedFiles = ENV.loadedFiles or {}
+  ENV.loadedFiles = loadedFiles
+  
+  local dofile = function( _sFile )
+    local fnFile, e = loadfile( _sFile )
+    if fnFile then
+      setfenv( fnFile, getfenv(3) )
+      return fnFile()
     else
-      error("file not found: "..file)
+      error( e, 2 )
     end
   end
+  
+  local function loadFile(file)
+    local modules = {dofile(file)}
+    loadedFiles[file] = {modules}
+    return unpack(modules)
+  end
+  
+  if not ENV.loadedFiles then
+    loadedFiles = {} end
+  local file = shell.resolve(name)
+  if loadedFiles[file] then
+    return unpack(ENV.loadedFiles[file])
+  end
+  if checkFileExists(file) then
+    return loadFile(file)
+  elseif checkFileExists(file..".lua") then
+    return loadFile(file..".lua")
+  else
+    for _,path in ipairs(USR_PATH) do
+      file = fs.combine(path,name)
+      if loadedFiles[file] then
+        return unpack(loadedFiles[file])
+      end
+      if checkFileExists(file) then
+        return loadFile(file)
+      elseif checkFileExists(file..".lua") then
+        return loadFile(file..".lua")
+      end
+    end
+  end
+  error("module '"..name.."' not found",2)
 end
 
 return require

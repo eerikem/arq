6e035ac8adf811c1ce60361198eb2deaecce6906
Eric <eric.nobody@gmail.com>
2018-01-01 23:57:46 -0500

    Fancy static UI for info panels

--- src/static_ui.lua
+++ src/static_ui.lua
@@ -18,7 +18,7 @@ function Status.start(monitor,titleStr,status,scale)
   
   ui.setTextScale(2)
   local w,h = ui.term.getSize()
-  if string.len(titleStr) > w then
+  if string.len(titleStr) > w / 2 then
       ui.setTextScale(1)
   end
   
@@ -48,6 +48,67 @@ function Status.start(monitor,titleStr,status,scale)
   end
   
   dark()
+--  VM.log(string.format("%s size: %d %d",monitor,ui.term.getSize()))
+  ui:update()  
+end
+
+---
+-- @function [parent=#src.static_ui] startFancy
+-- @param #string monitor
+-- @param #string title
+-- @param #string status optional secondary message
+function Status.startFancy(monitor,titleStr,status)
+  local ui = UI.start(monitor)
+  local title = Graphic:new(titleStr)
+  local body = Panel:new()
+  body.width = "max"
+  
+  ui.setTextScale(2)
+  local w,h = ui.term.getSize()
+  if string.len(titleStr) > w / 2 then
+      ui.setTextScale(1)
+  end
+  
+  if status and string.len(status) > w / 2 then
+    ui.setTextScale(1)
+  end
+  
+  title.width="max"
+  title:align("center")
+  ui:add(title)
+  
+  if status then
+    local status = Graphic:new(status)
+    status:align("center")
+    body:add(status)
+    body:add(Graphic:new("                  "))
+    ui:add(body)
+    status.width = "max"
+    status.ypos = 2
+  end  
+
+  local function dark()
+    ui:setText(colors.white)
+    ui:setBackground(colors.gray)
+    body:setTextColor(colors.gray)
+    body:setBackgroundColor(colors.lightGray)
+  end
+  
+  local function bright()
+    ui:setBackground(colors.lightGray)
+    ui:setText(colors.gray)
+    body:setBackgroundColor(colors.gray)
+    body:setTextColor(colors.lightGray)
+  end
+  
+  local function someColor()
+    ui:setBackground(colors.blue)
+    ui:setText(colors.white)
+    body:setBackgroundColor(colors.lightGray)
+    body:setTextColor(colors.gray)
+  end
+--  dark()
+  bright()
 --  VM.log(string.format("%s size: %d %d",monitor,ui.term.getSize()))
   ui:update()  
 end
